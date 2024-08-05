f5ffa6e6ff9013037691b17e809845e3ca5eba50
malefooo <31642966+malefooo@users.noreply.github.com>
2024-08-05 14:49:24 +0800

    第14节课 (#31)

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 200
+const MAX_SPEED = 125
+const ACCELERAATION_SMOOTHING = 25
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +12,10 @@ func _ready():
 func _process(delta):
 	var vec = get_movement_verctor()
 	var direction = vec.normalized()
-	velocity = direction * MAX_SPEED
+	
+	var target_velocity = direction * MAX_SPEED
+	
+	velocity = velocity.lerp(target_velocity, 1- exp(-delta * ACCELERAATION_SMOOTHING))
 	move_and_slide()
 
 
