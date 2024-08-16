5391fe2c46e9e7f4f9098cd5c39b473b8216dbb8
yazibuhuiyouyong <675870001@qq.com>
2024-08-16 18:36:08 +0800

    视频11 (#41)
    * 视频9.创建武器动画
    
    Signed-off-by: yazibuhuiyouyong <675870001@qq.com>
    
    * Merge remote-tracking branch 'origin/yzj-8' into yzj-8
    
    Signed-off-by: yazibuhuiyouyong <675870001@qq.com>
    
    * Merge remote-tracking branch 'origin/yzj-8' into yzj-8
    
    Signed-off-by: yazibuhuiyouyong <675870001@qq.com>
    
    ---------
    
    Signed-off-by: yazibuhuiyouyong <675870001@qq.com>

--- github2/scenes/basic_enemy/basic_enemy.gd
+++ github2/scenes/basic_enemy/basic_enemy.gd
@@ -4,6 +4,7 @@ const MAX_SPEED = 75
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Area2D.area_entered.connect(on_area_enntered)
 	pass # Replace with function body.
 
 
@@ -19,3 +20,7 @@ func get_direction_to_player():
 	if player_node !=null:
 		return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
+
+func on_area_enntered(other_area: Area2D):
+	queue_free()
+
