ca05855b3b64203014e55810602363012438fa54
malefooo <31642966+malefooo@users.noreply.github.com>
2024-08-26 17:26:50 +0800

    第23节课 (#45)

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -18,6 +18,7 @@ func on_level_up(cyrrent_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as  Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 func apply_upgrade(upgrade: AbilityUpgrade):
 
@@ -32,3 +33,5 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		current_upgrades[upgrade.id]["quantity"] += 1
 		
 	
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
