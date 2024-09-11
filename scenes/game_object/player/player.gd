0122c93d1aad57b9c54663f5bd3cb7109b7489fb
malefooo <784479660@qq.com>
2024-09-11 09:58:10 +0800

    第27节课

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -4,6 +4,8 @@ const MAX_SPEED = 125
 const ACCELERAATION_SMOOTHING = 25
 
 @onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
 
 var number_colliding_bodies = 0
 
@@ -12,6 +14,8 @@ func _ready():
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	update_health_display()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,6 +28,8 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1- exp(-delta * ACCELERAATION_SMOOTHING))
 	move_and_slide()
 
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
 
 func get_movement_verctor():
 	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move _left")
@@ -33,7 +39,7 @@ func get_movement_verctor():
 func check_deal_damage():
 	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
 		return
-	$HealthComponent.damage(1)
+	health_component.damage(1)
 	damage_interval_timer.start()
 
 func on_body_entered(other_body: Node2D):
@@ -45,3 +51,6 @@ func on_body_exited(other_body: Node2D):
 	
 func on_damage_interval_timer_timeout():
 	check_deal_damage()
+	
+func on_health_changed():
+	update_health_display()
